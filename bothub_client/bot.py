71a069cd112f3a3c4270d6ea8eca0ab7d5861a81
Jeongsoo Park <toracle@gmail.com>
2017-02-27 18:29:48 -0800

    remove unnecessary codes
    storage and channel client itself can handle null sender/channel case

--- bothub_client/bot.py
+++ bothub_client/bot.py
@@ -8,16 +8,12 @@ class BaseBot(object):
         self.channel_client = channel_client
         self.storage_client = storage_client
         self.event = event
-        self.current_user_id = event.get('sender') if event else None
-        self.current_channel = event.get('channel') if event else None
 
     def handle_message(self, event, context):
         raise NotImplementedError()
 
     def send_message(self, message, user_id=None, channel=None):
-        _user_id = user_id or self.current_user_id
-        _channel = channel or self.current_channel
-        self.channel_client.send_message(_user_id, message, _channel)
+        self.channel_client.send_message(user_id, message, channel)
 
     def set_project_data(self, data):
         self.storage_client.set_project_data(data)
@@ -26,11 +22,7 @@ class BaseBot(object):
         return self.storage_client.get_project_data()
 
     def set_user_data(self, data, user_id=None, channel=None):
-        _user_id = user_id or self.current_user_id
-        _channel = channel or self.current_channel
-        self.storage_client.set_user_data(data, user_id=_user_id, channel=_channel)
+        self.storage_client.set_user_data(data, user_id=user_id, channel=channel)
 
     def get_user_data(self, user_id=None, channel=None):
-        _user_id = user_id or self.current_user_id
-        _channel = channel or self.current_channel
-        return self.storage_client.get_user_data(user_id=_user_id, channel=_channel)
+        return self.storage_client.get_user_data(user_id=user_id, channel=channel)
