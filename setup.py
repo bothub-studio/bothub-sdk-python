8911a7951247e621fe2791639ab6235c8a57c4c8
Jeongsoo Park <toracle@gmail.com>
2017-06-20 22:06:29 +0900

    bump a version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='bothub',
-    version='0.1.12',
+    version='0.1.13',
     description=u'Build serverless chatbot on BotHub.Studio',
     long_description=LONG_DESCRIPTION,
     author='Jeongsoo Park',
