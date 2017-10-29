19fe7bf8be912a7f635a8c3f9ff01441bf204325
Jeongsoo Park <toracle@gmail.com>
2017-10-29 22:02:42 +0900

    refactoring
     - rename methods to make clear its purpose
     - move some logic to more relevant class
     - extract condition expression to a method and name it

--- bothub_client/dispatcher.py
+++ bothub_client/dispatcher.py
@@ -9,40 +9,68 @@ class DefaultDispatcher(object):
     command_handler_pattern = 'on_{command}'
 
     def __init__(self, bot, state):
+        '''Initializer
+
+        :param bot: a Bot object
+        :type bot: BaseBot
+        :param state: an IntentState object
+        :type state: IntentState
+        '''
         self.bot = bot
         self.state = state
 
     def dispatch(self, event, context):
+        '''Dispatch incoming message event.
+
+        :param event: an event object
+        :type event: dict
+        :param context: a context object
+        :type content: dict
+        '''
         logger.debug('dispatch: started')
-        data = self.bot.get_user_data()
-        if self.state.is_processing(data):
+        if self.state.is_opened():
             logger.debug('dispatch: continue to process intent')
             result = self.state.next(event)
             if result.completed:
                 logger.debug('dispatch: intent completed')
-                self.state.on_complete(result.intent_id, event, context, **result.answers)
+                handler_func = getattr(self.bot, result.complete_handler_name)
+                handler_func(**result.answers)
             else:
                 self.bot.send_message(result.next_message)
             return
 
         content = event['content']
 
-        if content.startswith('/intent '):
-            _, intent_id = content.split()
+        if self._is_intent_command(content):
+            intent_id = self._get_intent_id(content)
             logger.debug('dispatch: intent %s started', intent_id)
-            self.state.init(intent_id)
+            self.state.open(intent_id)
             result = self.state.next()
             self.bot.send_message(result.next_message)
             return
 
-        if content.startswith('/'):
-            tokens = content.split()
-            command = tokens[0][1:]
+        if self._is_command(content):
+            command, args = self._get_command_args(content)
             logger.debug('dispatch: start command %s', command)
-            args = tokens[1:]
-            func = getattr(self.bot, self.command_handler_pattern.format(command=command))
-            func(event, context, *args)
+            handler_func = getattr(self.bot, self.command_handler_pattern.format(command=command))
+            handler_func(event, context, *args)
             return
 
-        func = getattr(self.bot, self.default_handler_name)
-        func(event, context)
+        handler_func = getattr(self.bot, self.default_handler_name)
+        handler_func(event, context)
+
+    def _is_intent_command(self, content):
+        return content.startswith('/intent ')
+
+    def _get_intent_id(self, content):
+        _, intent_id = content.split()
+        return intent_id
+
+    def _is_command(self, content):
+        return content.startswith('/')
+
+    def _get_command_args(self, content):
+        tokens = content.split()
+        command = tokens[0][1:]
+        args = tokens[1:]
+        return command, args
